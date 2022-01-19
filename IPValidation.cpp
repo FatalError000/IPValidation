287d52493f8be454b4d36670976189c156928c50
Artur Svirsky <fatalerror2007@mail.ru>
2022-01-19 21:40:00 +0300

    Finally.

--- IPValidation.cpp
+++ IPValidation.cpp
@@ -1,20 +1,139 @@
-// IPValidation.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+string byteValidation(string byteS)
+{
+    string result = "";
+    int byteI;
+
+    for (int i = 0; i < byteS.length() - 1; i++)
+    {
+        if ((i > 0) && ((byteS[0] == '0') && (byteS[1] == '0')))
+        {
+            return result = "Invalid. Two or more zeros in a row detected!";
+        }
+
+        if ((i > 0) && (byteS[0] == '0'))
+        {
+            return result = "Invalid. Invalid position of zero!";
+        }
+    }
+    
+    byteI = stoi(byteS);
+
+    if (byteI > 255)
+    {
+        return result = "Invalid. The value of one of the bytes more than 255";
+    }
+
+    result = "Valid";
+
+    return result;
+}
+
+string ipValidation(string IPAdress)
+{
+    string result = "", byteS = "";
+    int dotsCounter = 0;
+    
+    for (int i = 0; i < IPAdress.length(); i++)
+    {
+        if (!((IPAdress[i] == '.') || ((IPAdress[i] >= '0') && (IPAdress[i] <= '9'))))
+        {
+            return result = "Invalid. Wrong symbols detected!";
+        }
+
+        if ((i == IPAdress.length() - 1) && (IPAdress[i] == '.'))
+        {
+            return result = "Invalid. Dot as last symbol detected!";
+        }
+
+        if ((i > 0) && ((IPAdress[i] == '.') && (IPAdress[i - 1] == '.')))
+        {
+            return result = "Invalid. Two or more dots in a row detected!";
+        }       
+
+        if (IPAdress[i] == '.')
+        {
+            dotsCounter++;
+        }
+
+        if ((i > 0) && (IPAdress[i] == '.'))
+        {
+            result = byteValidation(byteS);
+            byteS = "";
+
+            if (result[0] == 'I')
+            {
+                break;
+            }
+        }
+        else
+        {
+            byteS += IPAdress[i];
+            
+            if (i == IPAdress.length() - 2)
+            {
+                result = byteValidation(byteS);
+            }
+        }
+    }
+
+    if (dotsCounter != 3)
+    {
+        result = "Invalid. Incorrect amount of bytes.";
+    }
+
+    if (result == "") 
+    {
+        result = "Valid";
+    }    
+
+    return result;
+}
+
+void ipAdressCorrectTest()
+{
+    cout << "Correct IP adresses." << endl;
+    cout << "Enter your IP adress : 127.0.0.1" << endl;
+    cout << "Your IP adress is " << ipValidation("127.0.0.1") << endl;
+    cout << "Enter your IP adress : 255.255.255.255" << endl;
+    cout << "Your IP adress is " << ipValidation("255.255.255.255") << endl;
+    cout << "Enter your IP adress : 1.2.3.4" << endl;
+    cout << "Your IP adress is " << ipValidation("1.2.3.4") << endl;
+    cout << "Enter your IP adress : 55.77.213.101" << endl;
+    cout << "Your IP adress is " << ipValidation("55.77.213.101") << endl;
+}
+
+void ipAdressIncorrectTest()
+{
+    cout << endl << "Incorrect IP adresses." << endl;
+    cout << "Enter your IP adress : 255.256.257.258" << endl;
+    cout << "Your IP adress is " << ipValidation("255.256.257.258") << endl;
+    cout << "Enter your IP adress : 0.55.33.22." << endl;
+    cout << "Your IP adress is " << ipValidation("0.55.33.22.") << endl;
+    cout << "Enter your IP adress : 10.00.000.0" << endl;
+    cout << "Your IP adress is " << ipValidation("10.00.000.0") << endl;
+    cout << "Enter your IP adress : 23.055.255.033" << endl;
+    cout << "Your IP adress is " << ipValidation("23.055.255.033") << endl;
+    cout << "Enter your IP adress : 65.123..9" << endl;
+    cout << "Your IP adress is " << ipValidation("65.123..9") << endl;
+    cout << "Enter your IP adress : a.b.c.d" << endl;
+    cout << "Your IP adress is " << ipValidation("a.b.c.d") << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string IPAdress = "";
+
+    ipAdressCorrectTest();
+    ipAdressIncorrectTest();
+
+    cout << "Enter your IP adress : " << endl;
+    cin >> IPAdress;
+    cout << "Your IP adress is " << ipValidation(IPAdress) << endl;
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
